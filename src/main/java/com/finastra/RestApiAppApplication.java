bbd237c70d45bd22015966c47a7a055773dcff5c
vijji611 <71330954+vijji611@users.noreply.github.com>
2021-01-07 16:16:45 +0530

    Update RestApiAppApplication.java

--- src/main/java/com/finastra/RestApiAppApplication.java
+++ src/main/java/com/finastra/RestApiAppApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiAppApplication {
+public class RestApiAppApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(RestApiAppApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiAppApplication.class, args);
 	}
